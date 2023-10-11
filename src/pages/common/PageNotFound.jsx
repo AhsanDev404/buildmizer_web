24d9287e23462f9947ebf45e9bad1b9214dd9843
Muhammad Ahsan <ahsan.dev.404@gmail.com>
2023-10-11 22:06:20 +0500

    Update PageNotFound.jsx

--- src/pages/common/PageNotFound.jsx
+++ src/pages/common/PageNotFound.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Button, Center, Heading } from '@chakra-ui/react';
 
 export default function PageNotFound() {
   return (
-    <div>PageNotFound</div>
-  )
+    <Center minH="100vh">
+      <Box p={8} rounded="lg" borderWidth={1} boxShadow="lg" bgColor="white" w="300px" textAlign="center">
+        <Heading mb={4} fontSize="2xl" color="primary.500">
+          Page Not Found
+        </Heading>
+        <p>Oops! The page you are looking for does not exist.</p>
+        <Button as={Link} to="/" colorScheme="primary" width="full" mt={4}>
+          Go to Home
+        </Button>
+      </Box>
+    </Center>
+  );
 }
